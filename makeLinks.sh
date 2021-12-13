de082340887436ba20ffb6a624b3bf6c9bdede27
Yves Zoundi <yveszoundi@gmail.com>
2021-12-13 07:08:39 -0500

    Initial commit

---
+++ makeLinks.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -sf .config/readline/inputrc $HOME/.inputrc
+ln -sf .config/shell/okshrc $HOME/.kshrc
+ln -sf .config/shell/profile $HOME/.profile
+ln -sf .config/x11/xsessionrc $HOME/.xsessionrc
