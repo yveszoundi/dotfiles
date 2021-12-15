8bc6da00f784a6bc9255706c03dd47bc3b29cf46
Yves Zoundi <yveszoundi@gmail.com>
2021-12-14 19:06:12 -0500

    Adjustments for hyper-v
    - Setting the TERM variable to st-256color, seems to require registration in some distributions
    - Add history file in kshrc and remove the ctrl-L binding to clear the screen
    - Update makeLinks scrips

--- makeLinks.sh
+++ makeLinks.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
-ln -sf .config/readline/inputrc $HOME/.inputrc
-ln -sf .config/shell/okshrc $HOME/.kshrc
-ln -sf .config/shell/profile $HOME/.profile
-ln -sf .config/x11/xsessionrc $HOME/.xsessionrc
+mkdir -p $HOME/.config
+
+for f in $(ls $PWD/.config); do
+    ln -sf $PWD/$f $HOME/.config/
+done
+
+ln -sf $PWD/.config/readline/inputrc $HOME/.inputrc
+ln -sf $PWD/.config/shell/okshrc $HOME/.kshrc
+ln -sf $PWD/.config/shell/profile $HOME/.profile
+ln -sf $PWD/.config/x11/xsessionrc $HOME/.xsessionrc
+ln -sf $PWD/.config/x11/xsession $HOME/.xsession
+ln -sf $PWD/.config/x11/xsession $HOME/.initrc
+
+chmod +x $HOME/.xsession
+chmod +x $HOME/.xinitrc
+
